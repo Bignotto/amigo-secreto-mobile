15fb1c95f1e6f982084eaa35bf0f6855bcc8f4d3
Thiago Bignotto <bignotto@gmail.com>
2023-10-05 07:04:58 -0300

    profile screen save profile, home and profile screen navigation and update profile points

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,11 +3,11 @@ import AppScreenContainer from "@components/AppScreenContainer";
 import AppSpacer from "@components/AppSpacer";
 import Header from "@components/Header";
 import ProfileCompleteCard from "@components/ProfileCompleteCard";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StackParamList } from "@routes/Navigation.types";
 import supabase from "@services/supabase";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UserProfile } from "src/@types/UserProfile";
 
 export default function Home() {
@@ -18,61 +18,70 @@ export default function Home() {
   const [profileComplete, setProfileComplete] = useState(0);
   const [userProfile, setUserProfile] = useState<UserProfile>();
 
-  useEffect(() => {
-    async function loadProfile() {
-      try {
-        let { data, error } = await supabase
-          .from("users")
-          .select()
-          .eq("email", user?.primaryEmailAddress?.emailAddress);
-        if (error) {
-          console.log({ error });
-          return;
-        }
+  async function loadProfile() {
+    try {
+      let { data, error } = await supabase
+        .from("users")
+        .select()
+        .eq("email", user?.primaryEmailAddress?.emailAddress);
+      if (error) {
+        console.log({ error });
+        return;
+      }
 
-        if (data?.length === 0 && isSignedIn) {
-          let { data, error } = await supabase.from("users").insert([
-            {
-              id: userId,
-              email: user?.primaryEmailAddress?.emailAddress,
-              name: user?.fullName,
-            },
-          ]);
-          setUserProfile({
+      if (data?.length === 0 && isSignedIn) {
+        let { data, error } = await supabase.from("users").insert([
+          {
             id: userId,
-            email: `${user?.primaryEmailAddress?.emailAddress}`,
-            name: `${user?.fullName}`,
-          });
-          setProfileComplete(0);
+            email: user?.primaryEmailAddress?.emailAddress,
+            name: user?.fullName,
+          },
+        ]);
+        setUserProfile({
+          id: userId,
+          email: `${user?.primaryEmailAddress?.emailAddress}`,
+          name: `${user?.fullName}`,
+        });
+        setProfileComplete(0);
 
-          if (error) console.log({ error });
-          return;
-        }
+        if (error) console.log({ error });
+        return;
+      }
 
-        setUserProfile(data![0]);
+      setUserProfile(data![0]);
 
-        let profilePoints = 0;
-        const profile: UserProfile = data![0];
-        if (profile.clothesSize) profilePoints += 25;
-        if (profile.shoeSize) profilePoints += 25;
-        if (profile.likeThings) profilePoints += 25;
-        if (profile.dontLikeThings) profilePoints += 25;
+      let profilePoints = 0;
+      const profile: UserProfile = data![0];
+      if (profile.clothesSize) profilePoints += 25;
+      if (profile.shoeSize) profilePoints += 25;
+      if (profile.likeThings) profilePoints += 25;
+      if (profile.dontLikeThings) profilePoints += 25;
 
-        setProfileComplete(profilePoints);
-      } catch (error) {}
-    }
+      setProfileComplete(profilePoints);
+    } catch (error) {}
+  }
+
+  useEffect(() => {
     if (isSignedIn) loadProfile();
   }, [isSignedIn]);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadProfile();
+    }, [])
+  );
 
   return (
     <AppScreenContainer>
       <Header />
       <AppSpacer verticalSpace="xlg" />
-      <ProfileCompleteCard
-        avatarUrl={`${user?.imageUrl}`}
-        userName={`${user?.fullName}`}
-        percentCompeted={profileComplete}
-      />
+      {profileComplete < 100 && (
+        <ProfileCompleteCard
+          avatarUrl={`${user?.imageUrl}`}
+          userName={`${user?.fullName}`}
+          percentCompeted={profileComplete}
+        />
+      )}
     </AppScreenContainer>
   );
 }
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,42 +1,71 @@
 import { useUser } from "@clerk/clerk-expo";
 import AppAvatar from "@components/AppAvatar";
+import AppButton from "@components/AppButton";
 import AppInput from "@components/AppInput";
 import AppLogo from "@components/AppLogo";
 import AppScreenContainer from "@components/AppScreenContainer";
 import AppSpacer from "@components/AppSpacer";
 import AppText from "@components/AppText";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamList } from "@routes/Navigation.types";
 import supabase from "@services/supabase";
 import React, { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
 import { ActivityIndicator } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { FormContainer, TopScreenContainer } from "./styles";
 
 export default function Profile() {
   const { user } = useUser();
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
+
   const [isLoading, setIsLoading] = useState(true);
 
-  const { control, setValue, handleSubmit } = useForm();
+  const [clothesSize, setClothesSize] = useState("");
+  const [shoeSize, setShoeSize] = useState("");
+  const [likeThings, setLikeThings] = useState("");
+  const [dontLikeThings, setDontLikeThings] = useState("");
+
+  async function loadProfile() {
+    const { data, error } = await supabase
+      .from("users")
+      .select()
+      .eq("email", user?.primaryEmailAddress?.emailAddress);
+    if (error) {
+      console.log({ error });
+    }
+    if (data) {
+      setClothesSize(data[0].clothesSize);
+      setShoeSize(data[0].shoeSize);
+      setLikeThings(data[0].likeThings);
+      setDontLikeThings(data[0].dontLikeThings);
+    }
+    setIsLoading(false);
+  }
 
   useEffect(() => {
-    async function loadProfile() {
-      const { data, error } = await supabase
-        .from("users")
-        .select()
-        .eq("email", user?.primaryEmailAddress?.emailAddress);
-      if (error) {
-        console.log({ error });
-      }
-      if (data) {
-        setValue("clothesSize", data[0].clothesSize);
-        setValue("shoeSize", data[0].shoeSize);
-        setValue("likeThings", data[0].likeThings);
-        setValue("dontLikeThings", data[0].clothesSize);
-      }
-      setIsLoading(false);
-    }
     loadProfile();
   }, []);
+
+  async function handleSaveProfile() {
+    setIsLoading(true);
+    let { data, error } = await supabase
+      .from("users")
+      .update({
+        clothesSize,
+        shoeSize,
+        likeThings,
+        dontLikeThings,
+      })
+      .eq("id", user?.id);
+    if (error) {
+      console.log({ error });
+      setIsLoading(false);
+      return;
+    }
+
+    navigation.goBack();
+  }
 
   return (
     <AppScreenContainer>
@@ -55,50 +84,42 @@ export default function Profile() {
           </TopScreenContainer>
           <AppSpacer />
           <FormContainer>
-            <Controller
-              control={control}
-              name="clothesSize"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <AppInput
-                  label="Tamanho que você veste?"
-                  value={value}
-                  onChangeText={(value) => onChange(value)}
-                />
-              )}
+            <AppInput
+              label="Tamanho que você veste?"
+              value={clothesSize}
+              onChangeText={(text) => setClothesSize(text)}
+              placeholder="38 GG"
             />
-            <Controller
-              control={control}
-              name="shoeSize"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <AppInput
-                  label="Tamanho que calça?"
-                  value={value}
-                  onChangeText={(value) => onChange(value)}
-                />
-              )}
+
+            <AppInput
+              label="Que número você calça?"
+              value={shoeSize}
+              onChangeText={(value) => setShoeSize(value)}
+              placeholder="39/40"
             />
-            <Controller
-              control={control}
-              name="likeThings"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <AppInput
-                  label="Do que você gosta?"
-                  value={value}
-                  onChangeText={(value) => onChange(value)}
-                />
-              )}
+
+            <AppInput
+              label="Do que você gosta?"
+              value={likeThings}
+              onChangeText={(value) => setLikeThings(value)}
+              numberOfLines={4}
+              multiline
+              textAlignVertical="top"
+              placeholder="Azul, colecionáveis e chocolate!"
             />
-            <Controller
-              control={control}
-              name="dontLikeThings"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <AppInput
-                  label="Do que você não gosta?"
-                  value={value}
-                  onChangeText={(value) => onChange(value)}
-                />
-              )}
+
+            <AppInput
+              label="Do que você não gosta?"
+              value={dontLikeThings}
+              onChangeText={(value) => setDontLikeThings(value)}
+              numberOfLines={4}
+              multiline
+              textAlignVertical="top"
+              placeholder="Verde, cerveja e vinho, chinelo e pijama"
             />
+
+            <AppSpacer />
+            <AppButton title="Salvar" onPress={handleSaveProfile} />
           </FormContainer>
         </ScrollView>
       )}
