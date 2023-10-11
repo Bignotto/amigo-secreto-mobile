7416aa94765862fdbdf4a226c34af24204d9e818
Thiago Bignotto <bignotto@gmail.com>
2023-10-11 07:31:02 -0300

    search and search result itens list

--- src/screens/SearchGroup/index.tsx
+++ src/screens/SearchGroup/index.tsx
@@ -1,22 +1,111 @@
+import AppIconButton from "@components/AppIconButton";
+import { IconButton } from "@components/AppIconButton/styles";
 import AppScreenContainer from "@components/AppScreenContainer";
 import AppSpacer from "@components/AppSpacer";
+import AppText from "@components/AppText";
 import Header from "@components/Header";
 import { FontAwesome5 } from "@expo/vector-icons";
-import React from "react";
-import { IconWrapper, InputComponent, SearchInputContainer } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamList } from "@routes/Navigation.types";
+import supabase from "@services/supabase";
+import moment from "moment";
+import React, { useState } from "react";
+import { FriendsGroup } from "src/@types/FriendsGroup";
+import { useTheme } from "styled-components/native";
+import {
+  GroupLeft,
+  GroupMiddle,
+  GroupRight,
+  IconWrapper,
+  InputComponent,
+  SearchInputContainer,
+  SearchResultItem,
+  SearchResultsContainer,
+} from "./styles";
 
 export default function SearchGroup() {
-  //NEXT: search database for groups
+  const theme = useTheme();
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
+
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState<FriendsGroup[]>([]);
+
+  async function handleSearch() {
+    try {
+      const { data, error } = await supabase
+        .from("friends_groups")
+        .select()
+        .ilike("title", `%${searchText}%`);
+      if (error) {
+        console.log({ error });
+        return;
+      }
+      setSearchResults(data);
+    } catch (error) {}
+  }
+
   return (
     <AppScreenContainer>
       <Header />
       <AppSpacer verticalSpace="xlg" />
       <SearchInputContainer>
-        <InputComponent />
+        <InputComponent
+          value={searchText}
+          onChangeText={(text) => setSearchText(text)}
+        />
         <IconWrapper>
-          <FontAwesome5 name="search" size={24} color="black" />
+          <IconButton onPress={handleSearch}>
+            <FontAwesome5 name="search" size={24} color="black" />
+          </IconButton>
         </IconWrapper>
       </SearchInputContainer>
+      <AppSpacer verticalSpace="xlg" />
+      <SearchResultsContainer>
+        {searchResults.length > 0 &&
+          searchResults.map((group) => (
+            <SearchResultItem key={group.id}>
+              <GroupLeft>
+                {group.drawn ? (
+                  <FontAwesome5
+                    name="check-circle"
+                    size={36}
+                    color={theme.colors.white}
+                  />
+                ) : (
+                  <FontAwesome5
+                    name="question-circle"
+                    size={36}
+                    color={theme.colors.white}
+                  />
+                )}
+              </GroupLeft>
+              <GroupMiddle>
+                <AppText bold size="lg" color={theme.colors.white}>
+                  {group.title}
+                </AppText>
+                <AppText color={theme.colors.white}>
+                  criado em: {moment(group.created_at).format("DD/MM/yyyy")}
+                </AppText>
+              </GroupMiddle>
+              <GroupRight>
+                <AppIconButton
+                  onPress={() =>
+                    navigation.navigate("FriendGroupDetails", {
+                      groupId: group.id,
+                    })
+                  }
+                >
+                  <FontAwesome5
+                    name="arrow-right"
+                    size={24}
+                    color={theme.colors.white}
+                  />
+                </AppIconButton>
+              </GroupRight>
+            </SearchResultItem>
+          ))}
+      </SearchResultsContainer>
     </AppScreenContainer>
   );
 }
--- src/screens/SearchGroup/styles.ts
+++ src/screens/SearchGroup/styles.ts
@@ -24,3 +24,29 @@ export const InputComponent = styled(TextInput).attrs<TextInput>({
   color: ${({ theme }) => theme.colors.text};
   flex: 1;
 `;
+
+export const SearchResultsContainer = styled.ScrollView``;
+
+export const SearchResultItem = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 4px;
+  margin-bottom: 4px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background-color: ${({ theme }) => theme.colors.shape_dark};
+  border-radius: 8px;
+`;
+
+export const GroupLeft = styled.View``;
+
+export const GroupMiddle = styled.View`
+  flex: 1;
+  margin-left: 16px;
+`;
+
+export const GroupRight = styled.View``;
